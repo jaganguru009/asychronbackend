b5d729df6df81979ad17a2594a04fd540cf494d7
jj <jaganguru009@gmail.com>
2021-06-01 15:04:51 +0530

    CORS fixes
    Signed-off-by: jj <jaganguru009@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var appRoot = require('app-root-path');
-var HTTP_PORT = 80;
-var HTTPS_PORT = 443;
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const appRoot = require('app-root-path');
+var cors = require('cors');
 
-var app = express();
+
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
+
+const app = express();
+app.use(cors())
 
 // app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '500mb' }));
@@ -16,28 +20,36 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '500mb', parameterLimit: 
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
-mongoose.connect("mongodb+srv://sa:Reset123@asychrondb.ctra5.mongodb.net/asychronDB?retryWrites=true&w=majority", {
 
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify:true,
-    keepAlive: true
+const a = false;
 
-}).then(()=>{
-    console.log("connection is succesfully connect ")
-}).catch((e)=>{
-    console.log("no connection"+e);
-}) 
+mongoose
+  .connect(
+    'mongodb+srv://sa:Reset123@asychrondb.ctra5.mongodb.net/asychronDB?retryWrites=true&w=majority',
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+      keepAlive: true,
+    },
+  )
+  .then(() => {
+    console.log('connection is succesfully connect ');
+  })
+  .catch((e) => {
+    console.log(`no connection${e}`);
+  });
 
 // logger.info(JSON.stringify(process.env, null, 2))
 
-// route requests by URI root 
+// route requests by URI root
 app.use('/api', require('./_api/routes'));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(process.env.PORT || 3000)
-module.exports = app
+app.listen(process.env.PORT || 3000);
+module.exports = app;
