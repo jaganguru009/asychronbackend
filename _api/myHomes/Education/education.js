938e7525e6d1cbe2167cbfd14a5896c2ebb180a0
jj <jaganguru009@gmail.com>
2021-06-06 21:36:45 +0530

    patch issue
    Signed-off-by: jj <jaganguru009@gmail.com>

--- _api/myHomes/Education/education.js
+++ _api/myHomes/Education/education.js
@@ -1,101 +1,86 @@
 var express = require("express");
-var appRoot =require('app-root-path');
+var appRoot = require('app-root-path');
 var educationService = require(appRoot + '/services/educationService');
-var router =express.Router();
+var router = express.Router();
 
 
 //get all educaion detail
 router.get("/", (req, res, next) => {
-    if (req.query.userName != undefined) {
-      educationService.getEduDetailByUserName(
-        req.query.userName,
-        (err, result) => {
-          if (err) {
-            res.status(500).send(err);
-          } else {
-            res.send({ 'EducationDetail': result });
-          }
-        }
-      );
-    } else {
-      educationService.getEduDetail("EducationDetail", (err, result) => {
+  if (req.query.userName != undefined) {
+    educationService.getEduDetailByUserName(
+      req.query.userName,
+      (err, result) => {
         if (err) {
           res.status(500).send(err);
         } else {
-          res.send(result);
+          res.send({ 'EducationDetail': result });
         }
-      });
-    }
-  });
-  
+      }
+    );
+  } else {
+    educationService.getEduDetail("EducationDetail", (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  }
+});
+
 
 
 
 //get education detail by id 
-router.get('/:id',(req,res,next)=>{
-    educationService.getEduDetailById(req.params.id,(err,result)=>{
-        if(err){
-            res.json(err);
-        }else{
-            res.json(result)
-        }
-    })
+router.get('/:id', (req, res, next) => {
+  educationService.getEduDetailById(req.params.id, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result)
+    }
+  })
 })
 
 //post education data
 router.post("/", (req, res, next) => {
-    educationService.getEduDetailByUserName(req.body.userName, (err, result) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        if (result.length > 0) {
-          console.log(JSON.stringify(result));
-          educationService.patchEduDetail(
-            result[0]._id,
-            req.body,
-            (err, result) => {
-              if (err) {
-                res.json(err);
-              } else {
-                res.json(result);
-              }
-            }
-          );
+  educationService.getEduDetailByUserName(req.body.userName, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      educationService.postEduDetail(req.body, (err, result) => {
+        if (err) {
+          res.json(err);
         } else {
-          educationService.postEduDetail(req.body, (err, result) => {
-            if (err) {
-              res.json(err);
-            } else {
-              res.json(result);
-            }
-          });
+          res.json(result);
         }
-      }
-    });
+      });
+    }
   });
-  
+});
+
 
 
 //update education detail
-router.patch('/:id',(req,res,next)=>{
-    educationService.patchEduDetail(req.params.id,req.body,(err,result)=>{
-        if(err){
-            res.json(err);
-        }else{
-            res.json(result);
-        }
-    })
+router.patch('/:id', (req, res, next) => {
+  educationService.patchEduDetail(req.params.id, req.body, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  })
 })
 
 //delete education data 
-router.delete('/:id',(req,res,next)=>{
-    educationService.deleteEdu(req.params.id,(err,result)=>{
-        if(err){
-            res.json(err);
-        }else{
-            res.json(result);
-        }
-    })
+router.delete('/:id', (req, res, next) => {
+  educationService.deleteEdu(req.params.id, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  })
 })
 
 
